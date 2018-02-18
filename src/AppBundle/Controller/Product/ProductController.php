d77e95e84d46ab5d5d6e7e481ef844e27f64d2ed
Babouze <ivorydu13@gmail.com>
2018-02-18 12:24:44 +0100

    feat(app): Added function to sort products array with user axes

--- src/AppBundle/Controller/Product/ProductController.php
+++ src/AppBundle/Controller/Product/ProductController.php
@@ -28,6 +28,80 @@ define('SHOP_EQUIPMENT', 4);
 
 class ProductController extends ControllerBase {
 
+    /*
+     * @param $products array of products, $user user to order with axes
+     * @return array
+     */
+    function orderProducts($products, $user) {
+        //NOTE: To change the criteria of sorting, you can change the order of foreach
+        $toProcess = $products; // Array of products to sort
+        $processed = array(); // Array of products sorted
+
+        // Process male or female
+        foreach ($toProcess as $key => $product) { // NOTE: Je n'ai pas oublié le cas où le male == female. Ces produits ne seront pas écartés par les 2 if et si les axes de l'utilisateur sont égaux, aucun des if n'écartera de produit
+            $userMale = false;
+            $userFemale = false;
+            if($user->getAxe()->getMale() > $user->getAxe()->getFemale())
+                $userMale = true;
+            else if($user->getAxe()->getMale() < $user->getAxe()->getFemale())
+                $userFemale = true;
+
+            if(($product->getAxe()->getMale() > $product->getAxe()->getFemale()) && $userFemale && !$userMale) { // If product is male and user is female
+                array_unshift($processed, $product); // We add this product to processed
+                unset($toProcess[$key]); // Remove the actual product
+            }
+            else if(($product->getAxe()->getMale() < $product->getAxe()->getFemale()) && !$userFemale && $userMale) { // If product is female and user is male
+                array_unshift($processed, $product); // We add this product to processed
+                unset($toProcess[$key]); // Remove the actual product
+            }
+        }
+
+        // Process sports
+        foreach ($toProcess as $key => $product) {
+            if($product->getAxe()->getSport() != $user->getAxe()->getSport()) {
+                array_unshift($processed, $product); // We add this product to processed
+                unset($toProcess[$key]); // Remove the actual product
+            }
+        }
+
+        // Process age
+        foreach ($toProcess as $key => $product) {
+            $ageMargin = 0.9;
+            if(($product->getAxe()->getAge() >= ($user->getAxe()->getAge() + $ageMargin)) && ($product->getAxe()->getAge() <= ($user->getAxe()->getAge() - $ageMargin))) { // If product age is not in user->age-0,9 <= product->age <= user->age+0,9
+                array_unshift($processed, $product); // We add this product to processed
+                unset($toProcess[$key]); // Remove the actual product
+            }
+        }
+
+        // Process CSP
+        foreach ($toProcess as $key => $product) {
+            $cspMargin = 10;
+            if(($product->getAxe()->getCsp() >= ($user->getAxe()->getCsp() + $cspMargin)) && ($product->getAxe()->getCsp() <= ($user->getAxe()->getCsp() - $cspMargin))) { // If product csp is not in user->csp-10 <= product->age <= user->csp+10
+                array_unshift($processed, $product); // We add this product to processed
+                unset($toProcess[$key]); // Remove the actual product
+            }
+        }
+
+        // Process brand
+        foreach ($toProcess as $key => $product) {
+            if($product->getAxe()->getBrand() != $user->getAxe()->getBrand()) {
+                array_unshift($processed, $product); // We add this product to processed
+                unset($toProcess[$key]); // Remove the actual product
+            }
+        }
+
+        // Process city (Reserved for future use)
+//        foreach ($toProcess as $key => $product) {
+//            if($product->getAxe()->getCity() != $user->getAxe()->getCity()) {
+//                array_unshift($processed, $product); // We add this product to processed
+//                unset($toProcess[$key]); // Remove the actual product
+//            }
+//        }
+
+        $processed = $toProcess + $processed; // We add last elements in first positions
+        return $processed; // Return ordered array
+    }
+
     /**
      * @ApiDoc(
      *      resource=true, section="Product",
